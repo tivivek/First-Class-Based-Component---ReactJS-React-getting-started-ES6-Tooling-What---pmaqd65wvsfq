547064c2f19c4fb63912804d2d25d891642c69f6
Vivek Tiwari <79350756+tivivek@users.noreply.github.com>
2023-01-16 01:13:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 
 class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe",
+      age: 34,
+      enrollmentNo: "12345678",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    document.getElementById("age").innerHTML =
+      parseInt(document.getElementById("age").innerHTML) + 1;
+  }
+  render() {
+    return (
+      <>
+        <h1>Hello, my name is {this.state.name}</h1>{" "}
+        <p>
+          I am <span id="age">{this.state.age}</span> years old and my
+          enrollment no is {this.state.enrollmentNo}
+        </p>
+        <button onClick={this.handleClick}>Increase Age</button>
+      </>
+    );
+  }
 }
-
 
 export default App;
